2f1e2e2336d646dd7adb673376d1f3089337c64f
frankMarcelino <frank.silva.ti@gmail.com>
2024-10-23 16:28:42 -0300

    feat: add barbershopitem component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,6 +1,6 @@
 import { MenuIcon } from "lucide-react"
-import { Card, CardContent } from "./card"
-import { Button } from "./button"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
 import Image from "next/image"
 
 const Header = () => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { SearchIcon } from "lucide-react"
-import Header from "./_components/ui/header"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/babershop-item"
 
-const home = () => {
+const home = async () => {
+  //chama meu DB
+  const barbershops = await db.barbershop.findMany({})
+  console.log(barbershops)
   return (
     <div>
       {/* hedaer */}
@@ -33,7 +38,10 @@ const home = () => {
         </div>
 
         {/* agendamento */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* esquerda */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -57,6 +65,14 @@ const home = () => {
             </div>
           </CardContent>
         </Card>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
