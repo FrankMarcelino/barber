96a4c54d9d2b99ed51314435a0f6e153fa6b6e20
frankMarcelino <frank.silva.ti@gmail.com>
2024-10-23 18:16:57 -0300

    feat: add bookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,11 @@
+interface QuickSearcOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearcOption[] = [
+  { imageUrl: "/cabelo.svg", title: "Cabelo" },
+  { imageUrl: "/barba.svg", title: "Barba" },
+  { imageUrl: "/acabamento.svg", title: "acabamento" },
+  { imageUrl: "/hidratacao.svg", title: "Hidratação" },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/babershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import { SearchIcon } from "lucide-react"
+import BookingItem from "./_components/booking-item"
 
 const home = async () => {
   //chama meu DB
@@ -34,19 +34,13 @@ const home = async () => {
         </div>
 
         {/* busca rapida */}
-        <div className="flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="" />
-            Barba
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/acabamento.svg" width={16} height={16} alt="" />
-            Acabamento
-          </Button>
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image src={option.imageUrl} width={16} height={16} alt="" />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -59,33 +53,7 @@ const home = async () => {
         </div>
 
         {/* agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage
-                    src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-                    alt="Avatar"
-                  />
-                </Avatar>
-                <p className="text-sm">Barbearia Vintage</p>
-              </div>
-            </div>
-            {/* direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Outubro</p>
-              <p className="text-2xl">23</p>
-              <p className="text-sm">12:28</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
