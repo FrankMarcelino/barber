c80a332d6151dee4809245983e1c20f9d85ec718
frankMarcelino <frank.silva.ti@gmail.com>
2025-02-11 14:19:24 -0300

    feat: add BookingItem component

---
+++ my-app/app/_constants/search.ts
@@ -0,0 +1,15 @@
+interface QuickSearchOption {
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    title: "Sobrancelhas",
+  },
+  {
+    title: "Labios",
+  },
+  {
+    title: "Cilios",
+  },
+]
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,13 @@
-import { Search, SearchIcon } from "lucide-react"
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   // chamar meu banco de dados
@@ -35,15 +35,11 @@ const Home = async () => {
 
         {/* Busca rapida */}
         <div className="mt-6 flex gap-3">
-          <Button className="gap-2" variant="secondary">
-            Sobrancelhas
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            Labios
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            Cilios
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* banner */}
@@ -52,31 +48,7 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Miicropigmentação</h3>
-              <div className="flex items-center gap-2">
-                <Avatar>
-                  <AvatarImage
-                    src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-                    alt="@shadcn"
-                  />
-                </Avatar>
-                <p className="text-sm">Dra. Aline</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2xl">10</p>
-              <p className="text-sm">08:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
